55de890d70d120f4b1e639448c3a45bbf220c7ed
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-03 01:17:33 +0400

    Implement Binary Search

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -4,12 +4,30 @@
     {
         static void Main(string[] args)
         {
+            int[] list = { 4, 6, 8, 10, 12, 14, 16, 18 };
 
+            Console.WriteLine(BinarySearch(list, 16));
+            Console.WriteLine(BinarySearch(list, 8));
+            Console.WriteLine(BinarySearch(list, 2));
         }
 
-        static int BinarySearch(int[] numbers, int number)
+        static int BinarySearch(int[] list, int target)
         {
-            return 0;
+            int left = 0;
+            int right = list.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (target == list[mid])
+                    return mid;
+                else if (target > list[mid])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return -1;
         }
     }
 }
