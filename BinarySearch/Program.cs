7b7394f876b1de2bfae39a7e5c04d5a09c6901aa
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-02 23:57:31 +0400

    Initialize the repo.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int BinarySearch(int[] numbers, int number)
+        {
+            return 0;
+        }
+    }
+}
