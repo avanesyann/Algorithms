bc037ed3a08bc52f81bde31b79ef7e35fbfec3a4
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-04 23:50:22 +0400

    Practice recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Countdown(10);
+            Console.WriteLine(Factorial(5));
+        }
+
+        static int Factorial(int n)
+        {
+            if (n <= 1) return 1;           // base case
+
+            return Factorial(n - 1) * n;    // recursive case
+        }
+        static void Countdown(int i)
+        {
+            if (i < 0) return;      // base case
+            Console.WriteLine(i);
+
+            Countdown(i - 1);       // recursive case
+        }
+    }
+}
