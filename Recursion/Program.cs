cc4ccb65c0168823a79a5ed783758ed40000b36f
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-05 22:51:45 +0400

    Practice more recursion.

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -6,8 +6,17 @@
         {
             Countdown(10);
             Console.WriteLine(Factorial(5));
+
+            Console.WriteLine(Staircase(5));
         }
 
+        static int Staircase(int n)
+        {
+            if (n < 0) return 0;        // base case
+            if (n == 0) return 1;       // base case
+
+            return Staircase(n - 1) + Staircase(n - 2) + Staircase(n - 3);      // recursive case
+        }
         static int Factorial(int n)
         {
             if (n <= 1) return 1;           // base case
