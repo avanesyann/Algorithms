75c5a1db9a9107cfe90a22cc400f46031c02008d
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-08 23:38:25 +0400

    Sum of array using Recursion

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -4,12 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Countdown(10);
-            Console.WriteLine(Factorial(5));
+            //Countdown(10);
+            //Console.WriteLine(Factorial(5));
 
-            Console.WriteLine(Staircase(5));
+            //Console.WriteLine(Staircase(5));
+
+            Console.WriteLine(ArraySum(new int[] { 12, 24, 36 }, 0));
         }
 
+        static int ArraySum(int[] array, int index)
+        {
+            if (array.Length == index) return 0;                // base case
+
+            return array[index] + ArraySum(array, index + 1);   // recursive case
+        }
         static int Staircase(int n)
         {
             if (n < 0) return 0;        // base case
