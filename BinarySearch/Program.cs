8786b0578b9b8f4e6c32ec957a82c24e1e91a6b1
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-09 23:03:23 +0400

    Binary Search Recursively

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -9,8 +9,20 @@
             Console.WriteLine(BinarySearch(list, 16));
             Console.WriteLine(BinarySearch(list, 8));
             Console.WriteLine(BinarySearch(list, 2));
+            Console.WriteLine(BinarySearchRecursive(list, 16, 0, list.Length - 1));
         }
 
+
+        static int BinarySearchRecursive(int[] list, int target, int left, int right)
+        {
+            if (left > right) return -1;
+
+            int mid = (left + right) / 2;
+
+            if (list[mid] == target) return mid;
+            else if (target > list[mid]) return BinarySearchRecursive(list, target, mid + 1, right);
+            else return BinarySearchRecursive(list, target, left, mid - 1);
+        }
         static int BinarySearch(int[] list, int target)
         {
             int left = 0;
