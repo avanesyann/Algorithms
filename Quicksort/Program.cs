789074a32d81b0309b3f1be47853d076bdb840d9
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-12 23:18:57 +0400

    Generate the pivot randomly

--- Quicksort/Program.cs
+++ Quicksort/Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
             int[] testArray = new int[] { 14, 24, 10, 9, 21, 19, 8, 18 };
+            int[] testArray2 = new int[] { 14, 24, 10 };
             int[] sortedArray = Quicksort(testArray);
+            int[] sortedArray2 = Quicksort(testArray2);
 
-            foreach (int i in sortedArray)
-            {
-                Console.Write(i + " ");
-            }
+            Console.WriteLine(string.Join(" ", sortedArray));
+            Console.WriteLine(string.Join(" ", sortedArray2));
         }
 
         static int[] Quicksort(int[] array)
@@ -19,21 +19,28 @@
             if (array.Length == 2)
                 return array[0] > array[1] ? new int[] { array[1], array[0] } : new int[] { array[0], array[1] };
 
-            int pivot = array[0];
+            Random rand = new Random();
+            int pivotIndex = rand.Next(array.Length);
+            int pivot = array[pivotIndex];
+
             List<int> left = new List<int>();
             List<int> right = new List<int>();
 
-            for (int i = 1; i < array.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
+                if (i == pivotIndex) continue;
+
                 if (array[i] < pivot)
                     left.Add(array[i]);
                 else right.Add(array[i]);
             }
 
-            int[] sortedLeft = Quicksort(left.ToArray());
-            int[] sortedRight = Quicksort(right.ToArray());
+            List<int> sorted = new List<int>();
+            sorted.AddRange(Quicksort(left.ToArray()));
+            sorted.Add(pivot);
+            sorted.AddRange(Quicksort(right.ToArray()));
 
-            return sortedLeft.Concat(new int[] { pivot }).Concat(sortedRight).ToArray();
+            return sorted.ToArray();
         }
     }
 }
