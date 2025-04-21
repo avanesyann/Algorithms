e9f0e080f3ef3616812d2db072fa30c02566f6b6
Narek Avanesyan <avanesyan12345@gmail.com>
2025-04-21 23:15:19 +0400

    Insertion Sort

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>() { 14, 8, 10, 9, 12, 19, 16, 6 };
+            List<int> sorted = InsertionSort(numbers);
+
+            Console.WriteLine(string.Join(" ", sorted));
+        }
+
+        static List<int> InsertionSort(List<int> nums)
+        {
+            for (int i = 1; i < nums.Count; i++)
+            {
+                int key = nums[i];
+                int j = i - 1;
+
+                while (j >= 0 && nums[j] > key)
+                {
+                    nums[j + 1] = nums[j];
+                    j--;
+                }
+                nums[j + 1] = key;
+            }
+
+            return nums;
+        }
+    }
+}
